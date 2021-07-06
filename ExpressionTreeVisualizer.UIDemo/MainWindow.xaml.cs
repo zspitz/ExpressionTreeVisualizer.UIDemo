56003275b82ad6fcbf72fbd0f969b3e646db649e
Zev Spitz <shivisi@hotmail.com>
2021-07-06 13:05:41 +0300

    Add project files.

---
+++ ExpressionTreeVisualizer.UIDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ExpressionTreeTestObjects;
+using ExpressionTreeVisualizer;
+using ExpressionTreeVisualizer.Serialization;
+using System.Linq;
+using System.Windows;
+using ExpressionTreeTestObjects.VB;
+
+namespace ExpressionTreeVisualizer.UIDemo {
+    public partial class MainWindow : Window {
+        private readonly ConfigViewModel configViewModel;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            configViewModel = new ConfigViewModel(new Config());
+
+            Loaded += (sender, e) => {
+                Loader.Load();
+                availableObjects.ItemsSource = Objects.Get()
+                    .Select(x => new TestObject(x))
+                    .OrderBy(x => x.Name)
+                    .ThenBy(x => x.Category)
+                    .ThenBy(x => x.Source)
+                    .ToArray();
+                settingsControl.DataContext = configViewModel;
+            };
+
+            availableObjects.SelectionChanged += (sender, e) => refresh();
+            configViewModel.PropertyChanged += (sender, e) => refresh();
+        }
+
+        private void refresh() {
+            if (availableObjects.SelectedValue is null) { return; }
+            var (selectedValue, config) = (
+                availableObjects.SelectedValue,
+                configViewModel.Model
+            );
+            var m = new VisualizerData(selectedValue, config);
+            visualizerControl.DataContext = new VisualizerDataViewModel(m);
+        }
+    }
+}
---
+++ ExpressionTreeVisualizer.UIDemo/ViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ExpressionTreeVisualizer.UIDemo {
+    public record TestObject(string Category, string Source, string Name, object O) {
+        public TestObject((string category, string source, string name, object o) x) : 
+            this(x.category, x.source, x.name, x.o) { }
+    }
+}
